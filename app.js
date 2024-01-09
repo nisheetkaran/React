615bc3dc5ce0ca0fc68a5d3a8ae5e4934dcb3592
nisheetkaran <nisheet.karan@iiitg.ac.in>
2024-01-10 02:23:11 +0530

    basic structure of webapp

--- app.js
+++ app.js
@@ -20,13 +20,71 @@ const Header = () => {
     )
 }
 
+const RestaurantCards = () => {
+    return (
+        <div className="cards">
+            <div className="card">
+                <div className="card-img">
+                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/nkj06c05iaqkfththgdn" alt="" />
+                </div>
+                <div className="card-body">
+                    <div className="card-title">
+                        <h3>Meghna Foods</h3>
+                    </div>
+                    <div className="card-text">
+                        <p>Cuisines</p>
+                        <p>4.4 Star</p>
+                        <p>32 Minutes</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+                <RestaurantCards/>
+            </div>
+        </div>
+    )
+}
+
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>© 2024 Restaurant Finder. All rights reserved.</p>
+        </div>
+    );
+}
+
+// In your Applayout component:
 const Applayout = () => {
-    return <div className="app">
-        // Header
-        <Header></Header>
-        //body
-        //footer
-    </div>
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
 }
 
 
