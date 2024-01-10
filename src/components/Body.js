695791b89312597fb2df6e5512d29405fa58de3a
nisheetkaran <nisheet.karan@iiitg.ac.in>
2024-01-11 04:54:02 +0530

    Filter logic implemented

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,28 @@
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
-
 const Body = () => {
-    return(
-    <div className = "body">
-        <div className ="search">Search</div> 
-        <div className= "res-container">
-            {resList.map((restaurant) => (
-            <RestaurantCard key={restaurant.id} resData={restaurant} />
-        ))}
-        </div> 
-    </div>
+    const [listOfRestraunts, setResList] = useState(resList);
+
+    return (
+        <div className="body">
+            <div className="filter">
+                <button className="filter-btn"
+                    onClick={() => {
+                        const filteredList = listOfRestraunts.filter(
+                            (res) => res.data.avgRating > 4
+                        );
+                        setResList(filteredList);
+                    }}>Top Rated Restaurants</button>
+            </div>
+            <div className="res-container">
+                {listOfRestraunts.map((restaurant) => (
+                    <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+                ))}
+            </div>
+        </div>
     );
 };
-
 
 export default Body;
